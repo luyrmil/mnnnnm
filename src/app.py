472c679bc921580b44db5bb735ae7f1f563878c6
luyrmil <limryul@gmail.com>
2026-04-29 13:23:38 +0900

    fixed ver

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from db import find_person
 
@@ -21,5 +22,5 @@ def search():
     return jsonify({"results": results, "error": None})
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":  
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
